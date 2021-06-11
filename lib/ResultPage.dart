fc2324af7969e47d2a41a0be7d717f53fff37ecf
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-06-11 19:38:24 +0530

    Result page

--- lib/ResultPage.dart
+++ lib/ResultPage.dart
@@ -7,6 +7,43 @@ class ResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title:"BMI Result".text.bold.center.make(),),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+              child:Container(
+                child: "Your Result".text.xl5.make(),
+              )
+          ),
+          Expanded(
+            flex: 6,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child:"sdfdsfsd".text.center.make(),
+              ),
+            ),
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>ResultPage()));
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.deepPurple,
+              ),
+              height: 80,
+              margin: EdgeInsets.only(top: 10),
+              width: double.infinity,
+
+              child: "Re-Calculate".text.center.xl5.bold.make(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
